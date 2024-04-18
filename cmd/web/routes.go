5e8244b385cf956a8ad4fffcbce4a1f2d3d621cd
Anatoli Weikum <weikum.anatoli@gmail.com>
2024-04-18 18:12:05 +0200

    add starting files for backend_bookings

---
+++ cmd/web/routes.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/toljaw/backend_bookings/pkg/config"
+	"github.com/toljaw/backend_bookings/pkg/handlers"
+
+	"net/http"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	//mux := pat.New()
+	//
+	//mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	//mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+	return mux
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
-module backend_bookings
+module github.com/toljaw/backend_bookings
 
-go 1.22
+go 1.22.1
+
+require (
+	github.com/alexedwards/scs/v2 v2.8.0 // indirect
+	github.com/go-chi/chi/v5 v5.0.12 // indirect
+	github.com/justinas/nosurf v1.1.1 // indirect
+)
